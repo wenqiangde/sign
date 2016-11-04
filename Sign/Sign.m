5959b41460d8215c72149587224a9bcdfe1939dc
张文强 <zwq562038677@163.com>
2016-11-04 18:54:11 +0800

    README
    添加README解说

--- Sign/Sign.m
+++ Sign/Sign.m
@@ -9,6 +9,7 @@
 #import "Sign.h"
 #import <CommonCrypto/CommonDigest.h>
 //lipo -create 路径1/libPrintTest.a 路径2/libPrintTest.a -output 路径3/libPrintTest.a
+//lipo -info 路径1/二进制文件 （参看二进制文件信息）
 
 @implementation Sign
 
